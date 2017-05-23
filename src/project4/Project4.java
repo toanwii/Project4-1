e85a4de164dc4246352147b737e4df616e4b1038
tienhuynh5312 <tien.huynh5312@gmail.com>
2017-05-23 13:35:33 -0700

    Add reading text file, and counting number of characters.

--- src/project4/Project4.java
+++ src/project4/Project4.java
@@ -5,17 +5,37 @@
  */
 package project4;
 
+import java.util.ArrayList;
+import Huffman.*;
 /**
  *
  * @author tienhuynh
  */
 public class Project4 {
+    static final int MAX_NUMBER_CHARS = 70000;
+    
+    private static int[] count = new int[MAX_NUMBER_CHARS];
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        String fileName = "Through_The_Looking_Glass.txt";
+        ArrayList<String> story = new ArrayList();
+        if (TextFileIO.hasFile(fileName)) {
+            TextFileIO.readFile(fileName);
+            story = TextFileIO.getTextFile();
+        }
+        for (String line : story) {
+            for (int i = 0; i < line.length(); i++) {
+                count[line.charAt(i)] += 1;
+            }
+
+        }
+        for (int i = 0; i < 128; i++) {
+            System.out.println(count[i]);
+        }
     }
-    
+
 }
