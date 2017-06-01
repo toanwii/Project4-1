8b9d80dc7fb5022ada6811de4bc86044e2d30970
TIEN HUYNH <tien.huynh5312@gmail.com>
2017-05-31 18:49:30 -0700

    New stucture for encoding

--- src/Huffman/HuffmanTree.java
+++ src/Huffman/HuffmanTree.java
@@ -35,36 +35,31 @@ public class HuffmanTree<T extends Comparable<? super T>>
      */
     public HuffmanTree(HuffmanData<T>[] dataArray) {
         // your code here
-        BinaryNode<HuffmanData<T>> tmp;
-        BinaryNode<HuffmanData<T>>[] nodes = new BinaryNode[dataArray.length];
+//        add(new BinaryNode<HuffmanData<T>>(dataArray[leafCount]),
+//                new BinaryNode<HuffmanData<T>>(dataArray[++leafCount]) );
+        BinaryNode<HuffmanData<T>>[] nodes;
+        nodes = new BinaryNode[dataArray.length];
         for (int i = 0; i < dataArray.length; i++) {
             nodes[i] = new BinaryNode<>(dataArray[i]);
         }
-        BinaryNode<HuffmanData<T>> prevRootNode;
-//        firstAdd(clone[leafCount], clone[++leafCount]);
         while (leafCount < nodes.length - 1) {
-//            prevRootNode = (BinaryNode<HuffmanData<T>>) getRootNode();
-//            firstAdd(clone[leafCount], clone[++leafCount]);
-            add(nodes[leafCount], nodes[++leafCount]);
+            firstAdd(dataArray[leafCount], dataArray[++leafCount]);
+            //nodes[leafCount] = (BinaryNode<HuffmanData<T>>) super.getRootNode();
             for (int i = leafCount; i < nodes.length - 1; i++) {
-                if (nodes[i].getData().getOccurances() >= getRootNode().getData().getOccurances()){
-//                    tmp = nodes[i];
-                    nodes[i] = (BinaryNode<HuffmanData<T>>) super.getRootNode();
+                if (getRootData().getOccurances() >= nodes[i].getData().getOccurances()) {
+                    nodes[i] = (BinaryNode<HuffmanData<T>>) getRootNode();
                     break;
-                }
-                else {
-                    nodes[i] = nodes[i+1];
+                } else {
+                    nodes[i] = nodes[i + 1];
                 }
             }
             //Tracking log. Can be deleted
-            System.out.println(super.getRootNode().getLeftChild().getData().getData() + ":" + super.getRootNode().getLeftChild().getData().getOccurances());
-            System.out.println(super.getRootNode().getRightChild().getData().getData() + ":" + super.getRootNode().getRightChild().getData().getOccurances());
-            System.out.println("====");
+            System.out.println(super.getRootNode().getLeftChild().getData().toString());
+            System.out.println(super.getRootNode().getRightChild().getData().toString());
             //End of Tracking log.
-//            Arrays.sort(nodes, leafCount, nodes.length);
+            System.out.println("====");
         }
-        System.out.println("Traverse");
-        this.inorderTraverse();
+
         keyMap = new TreeMap<String, T>();
         codeMap = new TreeMap<T, String>();
         setMaps(getRootNode(), "");
@@ -82,10 +77,9 @@ public class HuffmanTree<T extends Comparable<? super T>>
         leftTree.setRootNode(left);
         HuffmanTree<T> rightTree = new HuffmanTree<T>();
         rightTree.setRootNode(right);
-        /* This code is kept
+        /* This code is kept */
         setTree(new HuffmanData<T>(MARKER, left.getData().getOccurances() + right.getData().getOccurances()), leftTree, rightTree);
-         */
-        setTree(new HuffmanData<T>((T) (left.getData().getData().toString() + right.getData().getData().toString()), left.getData().getOccurances() + right.getData().getOccurances()), leftTree, rightTree);
+        //        setTree(new HuffmanData<T>((T) (left.getData().getData().toString() + right.getData().getData().toString()), left.getData().getOccurances() + right.getData().getOccurances()), leftTree, rightTree);
     }
 
     /**
@@ -98,31 +92,49 @@ public class HuffmanTree<T extends Comparable<? super T>>
     private void firstAdd(HuffmanData<T> element1, HuffmanData<T> element2) {
         BinaryNode<HuffmanData<T>> node1 = new BinaryNode<>(element1);
         BinaryNode<HuffmanData<T>> node2 = new BinaryNode<>(element2);
-        final BinaryNode<HuffmanData<T>> prevRootNode;
-        prevRootNode = (BinaryNode<HuffmanData<T>>) getRootNode();
 
-        if (node1.getData().getOccurances() < node2.getData().getOccurances()) {
+        if (getRootNode() == null) {
             add(node1, node2);
-        } else {
-            add(node2, node1);
+            return;
         }
 
+        BinaryNode<HuffmanData<T>> internalNode;
+        if (node1.getData().getOccurances() < node2.getData().getOccurances()) {
+            internalNode = new BinaryNode<>(
+                    new HuffmanData<>(MARKER, node1.getData().getOccurances() + node2.getData().getOccurances()),
+                    node1,
+                    node2);
+        } else {
+            internalNode = new BinaryNode<>(
+                    new HuffmanData<>(MARKER, node1.getData().getOccurances() + node2.getData().getOccurances()),
+                    node2,
+                    node1);
+        }
+        if (internalNode.getData().getOccurances() < getRootNode().getData().getOccurances()) {
+            add(internalNode, (BinaryNode<HuffmanData<T>>) super.getRootNode());
+        } else {
+            add((BinaryNode<HuffmanData<T>>) super.getRootNode(), internalNode);
+        }
     }
 
     /**
-     * add a single element to the tree smaller on the left NE.
+     * add a single element to the tree smaller on the left NEED REDOING.
      *
      * @param element1
      */
     private void add(HuffmanData<T> element1) {
         BinaryNode<HuffmanData<T>> node = new BinaryNode<>(element1);
-        final BinaryNode<HuffmanData<T>> prevRootNode;
-        prevRootNode = (BinaryNode<HuffmanData<T>>) getRootNode();
+        HuffmanTree<T> leftTree = new HuffmanTree<T>();
+        leftTree.setRootNode(node);
+        if (getRootNode() == null) {
+            setTree(new HuffmanData<T>(MARKER, node.getData().getOccurances()), leftTree, null);
+            return;
+        }
 
-        if (node.getData().getOccurances() < prevRootNode.getData().getOccurances()) {
-            add(node, prevRootNode);
+        if (getRootNode().getLeftChild() == null) {
+            add(node, (BinaryNode<HuffmanData<T>>) getRootNode().getRightChild());
         } else {
-            add(prevRootNode, node);
+            add((BinaryNode<HuffmanData<T>>) getRootNode().getLeftChild(), node);
         }
     }
 
